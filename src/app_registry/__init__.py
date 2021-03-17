7aabb87d9b17d7b705f0b9a7d2e809776c6f7ee8
Simon Adorf <carl.simon.adorf@gmail.com>
2021-03-17 10:56:34 +0100

    Add version module and constant.

--- src/app_registry/__init__.py
+++ src/app_registry/__init__.py
@@ -4,6 +4,7 @@
 from .core import AppRegistryData
 from .core import AppRegistrySchemas
 from .metadata import generate_apps_meta
+from .version import __version__
 from .web import build_from_config
 from .web import build_html
 from .web import write_schemas
@@ -12,6 +13,7 @@ from .web import write_schemas
 __all__ = [
     "AppRegistryData",
     "AppRegistrySchemas",
+    "__version__",
     "build_from_config",
     "build_html",
     "generate_apps_meta",
